adeb6627c4a9a65247e22989fca43e157c4f5c09
James Miller <james@jkmillertech.com>
2010-02-17 10:10:30 -0800

    Don't try to downcase twice

--- lib/smirk/category.rb
+++ lib/smirk/category.rb
@@ -5,7 +5,7 @@ module Smirk
       info.each do |key, value|
         instance_variable_set("@#{key}", value)
         Category.instance_eval do
-          attr_reader key.downcase.to_sym
+          attr_reader key.to_sym
         end
       end
       @session_id = session_id
